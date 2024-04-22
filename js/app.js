7cab1ad1704d75f396b1c14f733ff0c6cd8c89f3
indreshp135 <indreshp135@gmail.com>
2024-04-22 17:05:11 +0530

    feat: app

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ function loadJson() {
     .then((data) => {
       const categories = document.querySelector(".filters");
       let item = "";
+
+      categories.innerHTML = "";
       data.forEach((element) => {
         if (element.filter_name == "All categories") {
           element.products.forEach((element) => {
@@ -30,6 +32,65 @@ function categoryView(category) {
   `;
 }
 
+function addToCart(id) {
+  let cart = localStorage.getItem("cart");
+  fetch("data/products.json")
+    .then((responce) => responce.json())
+    .then((data) => {
+      var fullData = [];
+      data.forEach((element) => {
+        element.products.forEach((product) => {
+          fullData.push(product);
+        });
+      });
+      console.log(fullData);
+      const product = fullData.find((element) => element.id == id);
+      console.log(product);
+      if (cart) {
+        cart = JSON.parse(cart);
+        const index = cart.findIndex((element) => element.id == id);
+        if (index == -1) {
+          product.quantity = 1;
+          cart.push(product);
+        } else {
+          cart[index].quantity++;
+        }
+      } else {
+        product.quantity = 1;
+        cart = [product];
+      }
+      localStorage.setItem("cart", JSON.stringify(cart));
+      alert("Product added to cart");
+      document.querySelector(".cart-count").innerText = cart
+        .map((element) => element.quantity)
+        .reduce((a, b) => a + b, 0);
+      loadJson();
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
+
+function removeFromCart(id) {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    cart = JSON.parse(cart);
+    const index = cart.findIndex((element) => element.id == id);
+    if (index != -1) {
+      cart[index].quantity--;
+      if (cart[index].quantity == 0) {
+        cart.splice(index, 1);
+      }
+      localStorage.setItem("cart", JSON.stringify(cart));
+      alert("Product removed from cart");
+      document.querySelector(".cart-count").innerText = cart
+        .map((element) => element.quantity)
+        .reduce((a, b) => a + b, 0);
+      loadJson();
+    }
+  }
+}
+
 function productView(product) {
   return `
   <div class="col-3 product-item" data="${product.id}">
@@ -49,7 +110,45 @@ function productView(product) {
      <hr />
      <div class="product-content">
        <p class="product-price">${product.price}</p>
-       <button type="button" class="btn btn-add-cart"><span class="add-btn">+</span>&nbsp;Cart</button>
+       ${
+         localStorage.getItem("cart") &&
+         !JSON.parse(localStorage.getItem("cart")).find(
+           (element) => element.id == product.id
+         )
+           ? `<button type="button" onclick="addToCart(${product.id})" class="btn btn-add-cart">Add to cart</button>`
+           : `<button type="button" onclick="addToCart(${product.id})" class="btn btn-add-cart">
+        +
+       </button>`
+       }
+       ${
+         localStorage.getItem("cart") &&
+         JSON.parse(localStorage.getItem("cart")).find(
+           (element) => element.id == product.id
+         )
+           ? `<span class="quantity"> ${
+               JSON.parse(localStorage.getItem("cart")).find(
+                 (element) => element.id == product.id
+               ).quantity
+             }
+              </span>`
+           : ""
+       }
+       ${
+         localStorage.getItem("cart") &&
+         JSON.parse(localStorage.getItem("cart")).find(
+           (element) => element.id == product.id
+         )
+           ? `
+           <button
+             type="button"
+             onclick="removeFromCart(${product.id})"
+             class="btn btn-add-cart"
+           >
+             -
+           </button>
+           `
+           : ""
+       }
      </div>
  </div>
   `;
@@ -80,21 +179,8 @@ function categoryFilter(id) {
     });
 }
 
-// loader
-function removeLoader() {
-  $("#loadingDiv").fadeOut(500, () => {
-    $("#loadingDiv").remove();
-  });
-}
-
-$(window).on("load", () => {
-  setTimeout(removeLoader, 2000);
-
-  $("body").css(
-    "overflow-y",
-    "hidden",
-    setTimeout(() => {
-      $("body").css("overflow-y", "visible");
-    }, 2000)
-  );
-});
+document.querySelector(".cart-count").innerText = JSON.parse(
+  localStorage.getItem("cart") || "[]"
+)
+  .map((element) => element.quantity)
+  .reduce((a, b) => a + b, 0);
